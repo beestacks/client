c1387cbeeee5d98ae227f2dbf88b8a83f809d14a
Amoyensis <amoyensis@outlook.com>
2021-09-14 16:51:38 +0800

    fix bug

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -11,63 +11,16 @@ export class SidebarComponent implements OnInit {
   items: MenuItem[];
 
   constructor(private translate: TranslateService) {}
-  async ngOnInit() {
+  ngOnInit() {
     this.items = [
       {
-        label: await this.translate.get('sidebar.dashboard').toPromise(),
+        label: this.translate.instant('sidebar.dashboard'),
         routerLink: '/dashboard'
       },
       {
         label: 'home',
         routerLink: '/home',
-      },
-      {
-        label: 'Help',
-        icon: 'pi pi-fw pi-question',
-        items: [
-          {
-            label: 'Contents',
-            icon: 'pi pi-pi pi-bars',
-          },
-          {
-            label: 'Search',
-            icon: 'pi pi-pi pi-search',
-            items: [
-              {
-                label: 'Text',
-                items: [
-                  {
-                    label: 'Workspace',
-                  },
-                ],
-              },
-              {
-                label: 'User',
-                icon: 'pi pi-fw pi-file',
-              },
-            ],
-          },
-        ],
-      },
-      {
-        label: 'Actions',
-        icon: 'pi pi-fw pi-cog',
-        items: [
-          {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-pencil',
-            items: [
-              { label: 'Save', icon: 'pi pi-fw pi-save' },
-              { label: 'Update', icon: 'pi pi-fw pi-save' },
-            ],
-          },
-          {
-            label: 'Other',
-            icon: 'pi pi-fw pi-tags',
-            items: [{ label: 'Delete', icon: 'pi pi-fw pi-minus' }],
-          },
-        ],
-      },
+      }
     ];
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,19 +8,17 @@ import { WebviewDirective } from './directives/';
 import { FormsModule } from '@angular/forms';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelMenuModule } from 'primeng/panelmenu';
-import { SidebarModule } from 'primeng/sidebar';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [PageNotFoundComponent, WebviewDirective, NavbarComponent, SidebarComponent],
-  imports: [CommonModule, TranslateModule, FormsModule, MenubarModule, SidebarModule, PanelMenuModule],
+  imports: [CommonModule, TranslateModule, FormsModule, MenubarModule, PanelMenuModule],
   exports: [
     TranslateModule,
     WebviewDirective,
     FormsModule,
     MenubarModule,
     PanelMenuModule,
-    SidebarModule,
     NavbarComponent,
     SidebarComponent,
   ],
