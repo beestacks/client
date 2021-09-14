98da5c9de5f09f0ad4526ef7e3e3b6802a274c65
Amoyensis <amoyensis@outlook.com>
2021-09-14 16:12:13 +0800

    add animations

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,15 +1,77 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  items: MenuItem[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private translate: TranslateService) {}
+  ngOnInit() {
+    this.items = [
+      {
+        label: this.translate.instant('sidebar.dashboard'),
+        routerLink: '/dashboard'
+      },
+      {
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' },
+        ],
+      },
+      {
+        label: 'Help',
+        icon: 'pi pi-fw pi-question',
+        items: [
+          {
+            label: 'Contents',
+            icon: 'pi pi-pi pi-bars',
+          },
+          {
+            label: 'Search',
+            icon: 'pi pi-pi pi-search',
+            items: [
+              {
+                label: 'Text',
+                items: [
+                  {
+                    label: 'Workspace',
+                  },
+                ],
+              },
+              {
+                label: 'User',
+                icon: 'pi pi-fw pi-file',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Actions',
+        icon: 'pi pi-fw pi-cog',
+        items: [
+          {
+            label: 'Edit',
+            icon: 'pi pi-fw pi-pencil',
+            items: [
+              { label: 'Save', icon: 'pi pi-fw pi-save' },
+              { label: 'Update', icon: 'pi pi-fw pi-save' },
+            ],
+          },
+          {
+            label: 'Other',
+            icon: 'pi pi-fw pi-tags',
+            items: [{ label: 'Delete', icon: 'pi pi-fw pi-minus' }],
+          },
+        ],
+      },
+    ];
   }
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,11 +7,20 @@ import { PageNotFoundComponent, NavbarComponent } from './components/';
 import { WebviewDirective } from './directives/';
 import { FormsModule } from '@angular/forms';
 import { MenubarModule } from 'primeng/menubar';
+import { PanelMenuModule } from 'primeng/panelmenu';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [PageNotFoundComponent, WebviewDirective, NavbarComponent, SidebarComponent],
-  imports: [CommonModule, TranslateModule, FormsModule, MenubarModule],
-  exports: [TranslateModule, WebviewDirective, FormsModule, MenubarModule, NavbarComponent, SidebarComponent]
+  imports: [CommonModule, TranslateModule, FormsModule, MenubarModule, PanelMenuModule],
+  exports: [
+    TranslateModule,
+    WebviewDirective,
+    FormsModule,
+    MenubarModule,
+    PanelMenuModule,
+    NavbarComponent,
+    SidebarComponent,
+  ],
 })
 export class SharedModule {}
